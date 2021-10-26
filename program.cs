0a87b26d853ffa819eb9a28dd1f003430036d87e
Gustavogba <gustavopgb21@gmail.com>
2021-10-26 02:02:16 -0400

    nuevo de gus

--- program.cs
+++ program.cs
@@ -7,7 +7,8 @@ namespace GIT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("a darle con todo");
+            Console.WriteLine("a darle con todo siiiiii");
+
             Console.WriteLine("hola mike de una papu");
         }
     }
